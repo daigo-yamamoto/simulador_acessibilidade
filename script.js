4fa7f92c2525b6f85e75b35b73a5cb9ffb29901a
Bruno Yamamoto <bruno.daigo.yamamoto@gmail.com>
2023-11-22 15:13:29 -0300

    implementacao da movimentacao, mas o ceu sumiu, tamo de noite

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let scene, camera, renderer, ramp, stairs;
+let ramp, stairs;
+let camera, renderer, scene;
+let moveForward = false;
+let moveBackward = false;
+let rotateLeft = false;
+let rotateRight = false;
+let velocity = new THREE.Vector3();
+let rotationSpeed = 0.02; // velocidade da rotação
+let prevTime = performance.now();
 
 function init() {
     // Criar cena
@@ -14,14 +22,21 @@ function init() {
     document.getElementById('scene-container').appendChild(renderer.domElement);
 
     // Adicionar plano para grama
-    const grassTexture = new THREE.TextureLoader().load('texturas/grama.jpg'); // Substitua pelo caminho da sua textura
+    const grassTexture = new THREE.TextureLoader().load('texturas/grama.jpg'); 
     const grassMaterial = new THREE.MeshBasicMaterial({ map: grassTexture });
-    const grassGeometry = new THREE.PlaneGeometry(100, 100); // Aumente o tamanho conforme necessário
+    const grassGeometry = new THREE.PlaneGeometry(500, 500); // Aumente o tamanho conforme necessário
     grass = new THREE.Mesh(grassGeometry, grassMaterial);
     grass.rotation.x = -Math.PI / 2; // Rotação para deixar o plano horizontal
     grass.position.y = -0.5; // Ajuste a posição y conforme necessário
     scene.add(grass);
 
+    // Adicionar esfera para o céu
+    const skyTexture = new THREE.TextureLoader().load('texturas/ceu.jpg'); 
+    const skyMaterial = new THREE.MeshBasicMaterial({ map: skyTexture, side: THREE.BackSide });
+    const skyGeometry = new THREE.SphereGeometry(500, 32, 32);
+    sky = new THREE.Mesh(skyGeometry, skyMaterial);
+    scene.add(sky);
+    
     // Adicione luz para melhorar a visualização das texturas
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
@@ -29,12 +44,6 @@ function init() {
     directionalLight.position.set(0, 10, 0);
     scene.add(directionalLight);
 
-    // Adicionar esfera para o céu
-    const skyTexture = new THREE.TextureLoader().load('texturas/ceu.jpg'); // Substitua pelo caminho da sua textura
-    const skyMaterial = new THREE.MeshBasicMaterial({ map: skyTexture, side: THREE.BackSide });
-    const skyGeometry = new THREE.SphereGeometry(500, 32, 32);
-    sky = new THREE.Mesh(skyGeometry, skyMaterial);
-    scene.add(sky);
 
     // Adicionar rampa
     const rampGeometry = new THREE.BoxGeometry(5, 1, 10);
@@ -51,35 +60,86 @@ function init() {
     stairs.position.set(5, 0, 0);
     scene.add(stairs);
 
-    //Adicionando contorno nos objetos
-    addOutlineToObject(ramp, 0x000000, 0.05); // Adiciona um contorno preto com uma espessura de 5%
-    addOutlineToObject(stairs, 0x000000, 0.05); // Adiciona um contorno preto com uma espessura de 5%
+    // Posicao da camera de um cadeirante
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+    camera.position.set(0, 1.6, 0); // Altura da câmera simula a altura de uma pessoa sentada
 
+    // Event listeners para teclado
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
 
     // Animação (loop de renderização)
     animate();
 }
 
-function addOutlineToObject(object, color, thickness) {
-    const outlineMaterial = new THREE.MeshBasicMaterial({ 
-        color: color, 
-        side: THREE.BackSide 
-    });
-    const outline = new THREE.Mesh(object.geometry, outlineMaterial);
-    outline.position.copy(object.position);
-    outline.rotation.copy(object.rotation);
-    outline.scale.multiplyScalar(1 + thickness);
-    scene.add(outline);
+// Função para atualizar a posição da câmera
+function moveCamera(deltaTime) {
+    velocity.z -= velocity.z * 10.0 * deltaTime;
+
+    if (moveForward) velocity.z -= 400.0 * deltaTime;
+    if (moveBackward) velocity.z += 400.0 * deltaTime;
+
+    camera.translateZ(velocity.z * deltaTime);
+
+    if (rotateLeft) camera.rotation.y += rotationSpeed;
+    if (rotateRight) camera.rotation.y -= rotationSpeed;
 }
 
-
-
+// Função de animação
 function animate() {
     requestAnimationFrame(animate);
 
-    // Aqui você pode adicionar lógica para mover o cadeirante
+    const time = performance.now();
+    const deltaTime = (time - prevTime) / 1000;
+
+    moveCamera(deltaTime);
 
     renderer.render(scene, camera);
+
+    prevTime = time;
+}
+
+// Eventos de teclado
+function onKeyDown(event) {
+    switch (event.code) {
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            rotateLeft = true; 
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            rotateRight = true;
+            break;
+    }
+}
+
+function onKeyUp(event) {
+    switch (event.code) {
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            rotateLeft = false;
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            rotateRight = false;
+            break;
+    }
 }
 
 // Iniciar a cena
